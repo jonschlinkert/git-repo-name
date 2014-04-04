b733127f04b1192a1309e71dbac49c2ca9bdb272
jonschlinkert <dev@sellside.com>
2014-04-04 12:43:47 -0400

    first commit

---
+++ docs/README.tmpl.md
@@ -0,0 +1,24 @@
+# {%= name %} {%= badge("fury") %}
+
+> {%= description %}
+
+## Install
+{%= include("install") %}
+
+## Usage
+
+```js
+var name = require('git-repo-name');
+console.log(name);
+```
+
+## Author
+{%= contrib("jon") %}
+
+## License
+{%= copyright() %}
+{%= license() %}
+
+***
+
+{%= include("footer") %}
---
+++ index.js
@@ -0,0 +1,27 @@
+/*!
+ * git-username <https://github.com/jonschlinkert/git-username>
+ *
+ * Copyright (c) 2014 Jon Schlinkert
+ * Licensed under the MIT License (MIT)
+ */
+
+const url = require('url');
+const origin = require('remote-origin-url');
+
+
+/**
+ * Get the remote origin url for a local git repository
+ */
+
+var remoteOriginURL = origin.url().replace(/\.git$/, '');
+
+
+/**
+ * Get the repository name from the GitHub remote origin URL
+ */
+
+module.exports = (function() {
+  return url.parse(remoteOriginURL).path.split('/')[0];
+})();
+
+console.log(module.exports)
