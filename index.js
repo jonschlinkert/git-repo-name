d48adbd6fd6c45251a798c1a84b7e2d192305459
jonschlinkert <dev@sellside.com>
2014-05-15 13:23:18 -0400

    v0.2.0
    improve error handling and logging

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * git-repo-name <https://github.com/jonschlinkert/git-repo-name>
+ * git-username <https://github.com/jonschlinkert/git-username>
  *
  * Copyright (c) 2014 Jon Schlinkert
  * Licensed under the MIT License (MIT)
@@ -8,12 +8,17 @@
 const url = require('url');
 const origin = require('remote-origin-url');
 const gitUrl = require('github-url-from-git');
-
+const log = require('verbalize');
 
 /**
  * Get the repository name from the GitHub remote origin URL
  */
 
 module.exports = (function() {
+  if (/\bhas not been defined\b/.test(origin.url())) {
+    log.warn("  Can't calculate git-repo-name. This probably means that");
+    log.warn("  a git remote origin has not been defined.");
+    return '';
+  }
   return url.parse(gitUrl(origin.url())).path.split('/')[2];
 })();
