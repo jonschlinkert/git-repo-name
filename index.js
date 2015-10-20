0efb3cb94be52d62849138d00fcf033560bc431a
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-20 04:07:22 -0400

    adds async/sync versions

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 'use strict';
 
 var url = require('url');
-var chalk = require('chalk');
-var origin = require('remote-origin-url');
+var path = require('path');
+var utils = require('./utils');
 
-module.exports = function repo(cwd, verbose) {
-  var github = origin.sync(cwd);
+module.exports = function(dir, cb) {
+  if (typeof dir === 'function') {
+    cb = dir;
+    dir = '';
+  }
 
-  if (!github) {
-    if (verbose) {
-      console.error(chalk.red('Can\'t find .git/config.'));
-    }
+  utils.origin(utils.cwd(dir), function (err, giturl) {
+    if (err) return cb(err);
+
+    var parsed = url.parse(giturl);
+    var segments = parsed.pathname.split(path.sep);
+    cb(null, utils.filename(segments.pop()));
+  });
+};
+
+module.exports.sync = function(dir) {
+  var giturl = utils.origin.sync(utils.cwd(dir));
+  if (!giturl) {
+    console.error(utils.red('cannot find ".git/config"'));
     return null;
   }
-
-  var parsed = url.parse(github);
-  var res = '';
-
-  if (parsed && parsed.path) {
-    if (parsed.path.charAt(0) === '/') {
-      parsed.path = parsed.path.slice(1);
-    }
-    res = parsed.path.replace(/\.git$/, '');
-  }
-
-  var i = res.indexOf('/');
-
-  if (res.length && i !== -1) {
-    return res.slice(i + 1);
-  }
-
-  return null;
+  var parsed = url.parse(giturl);
+  var segments = parsed.pathname.split(path.sep);
+  return utils.filename(segments.pop());
 };
